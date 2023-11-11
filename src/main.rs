63b527506c6d8d7016f68cb0d78b9c491ed668eb
ZhangIvan1 <zhang_ivan1@163.com>
2023-11-11 10:45:58 +0000

    :construction: Error Handling

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,26 @@ fn to_ctrl_byte(c: char) -> u8{
 }
 
 fn die(e: std::io::Error){
-    panic!(e);
+    panic!("{}", e);
 }
 
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes(){
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control(){
-            println!("{:?} \r", b);
-
-            //println!("{:#b} \r", b);
-            //println!("{:#b} \r", b & 0b0001_1111);
-        } else {
-            println!("{:?} ({})\r", b, c);
-        }
-        if b == to_ctrl_byte('q') {
-            break;
+        match b{
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?} \r", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+                if b == to_ctrl_byte('q'){
+                    break;
+                }
+            }
+            Err(err) => die(err),
         }
     }
 }
