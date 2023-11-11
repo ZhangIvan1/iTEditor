1846475f2b6480ca9db452feb15825197bb8491b
ZhangIvan1 <zhang_ivan1@163.com>
2023-11-11 10:15:21 +0000

    :construction: Add die funtion

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn to_ctrl_byte(c: char) -> u8{
     byte & 0b0001_1111
 }
 
+fn die(e: std::io::Error){
+    panic!(e);
+}
+
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
@@ -16,6 +20,9 @@ fn main() {
         let c = b as char;
         if c.is_control(){
             println!("{:?} \r", b);
+
+            //println!("{:#b} \r", b);
+            //println!("{:#b} \r", b & 0b0001_1111);
         } else {
             println!("{:?} ({})\r", b, c);
         }
