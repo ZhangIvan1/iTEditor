0eb076e0915f1e09a8c4f0c8e15a6f5fc2ab5384
ZhangIvan1 <zhang_ivan1@163.com>
2023-11-15 08:40:00 +0000

    :construction: Separate the code into multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,11 @@
 #![allow(non_snake_case)]
 
-use std::io::{self, stdout};
-use termion::raw::IntoRawMode;
-use termion::event::Key;
-use termion::input::TermRead;
+mod editor;
 
-
-fn die(e: std::io::Error){
-    panic!("{}", e);
-}
+use editor::Editor;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-    
-    for key in io::stdin().keys(){
-        match key{
-            Ok(key) => match key{
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?}\r", c as u8);
-                    } else {
-                        println!("{:?} ({}) \r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => die(err),
-        }
-    }
+    let editor = Editor {};
+    editor.run();
+
 }
