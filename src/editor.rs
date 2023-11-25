efe4f9e96d00fc5a24d32f90cf24d01575efaf12
ZhangIvan1 <zhang_ivan1@163.com>
2023-11-25 07:09:07 +0000

    :construction: A Hello world row

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,7 @@
+use crate::Document;
+use crate::Row;
 use crate::Terminal;
 use termion::event::Key;
-use crate::Document;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -101,9 +102,11 @@ impl Editor {
 
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for row in 0..height - 1 {
+        for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
-            if row == height / 3 {
+            if let Some(row) = self.document.row(terminal_row as usize) {
+                self.draw_row(row);
+            } else if terminal_row == height / 3 {
                 self.draw_welcome_message();
             } else {
                 println!("~\r")
@@ -120,6 +123,13 @@ impl Editor {
         welcome_message = format!("~{}{}", spaces, welcome_message);
         welcome_message.truncate(width);
         println!("{}\r", welcome_message);
+    }
+
+    pub fn draw_row(&self, row: &Row) {
+        let start = 0;
+        let end = self.terminal.size().width as usize;
+        let row = row.render(start, end);
+        println!("{}\r", row)
     }
 }
 
