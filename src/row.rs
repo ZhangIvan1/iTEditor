d208de2abb5b1071d497ffa63c2aeb8dbc17659e
ZhangIvan1 <zhang_ivan1@163.com>
2023-11-30 06:46:28 +0000

    :construction: Insert char

--- src/row.rs
+++ src/row.rs
@@ -1,6 +1,7 @@
 use std::cmp;
 use unicode_segmentation::UnicodeSegmentation;
 
+#[derive(Default)]
 pub struct Row {
     string: String,
     len: usize,
@@ -43,5 +44,18 @@ impl Row {
     }
     pub fn update_len(&mut self) {
         self.len = self.string[..].graphemes(true).count();
+    }
+
+    pub fn insert(&mut self, at: usize, c: char) {
+        if at >= self.len() {
+            self.string.push(c);
+        } else {
+            let mut resule: String = self.string[..].graphemes(true).take(at).collect();
+            let remainder: String = self.string[..].graphemes(true).skip(at).collect();
+            resule.push(c);
+            resule.push_str(&remainder);
+            self.string = resule;
+        }
+        self.update_len();
     }
 }
